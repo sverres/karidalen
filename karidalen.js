707c9e68e50b03c1287792c7dff839f56b6b298d
Sverre Stikbakke <sverre.stikbakke@gmail.com>
2016-03-27 14:31:26 +0200

    Refaktorering på zoom-nivåer

--- karidalen.js
+++ karidalen.js
@@ -1,7 +1,7 @@
 /*
  * http://github.com/sverres
  *
- * sverre.stikbakke 22.03.2016
+ * sverre.stikbakke 27.03.2016
  *
  */
 
@@ -26,41 +26,73 @@ var projection = new ol.proj.Projection({
   extent: extent150km
 });
 
-resolutions = [
+var resolutionsKartverket = [
+  21664,
+  10832,
+  5416,
+  2708,
+  1354,
   677,
-  //338.5,
+  338.5,
   169.25,
-  //84.625,
+  84.625,
   42.3125,
-  //21.15625,
+  21.15625,
   10.578125,
-  //5.2890625,
+  5.2890625,
   2.64453125,
   1.322265625,
   0.6611328125,
   0.33056640625,
-  //0.165283203125
+  0.165283203125
 ];
-
-var switchLayerResolution = resolutions[4];
 
 var matrixSet = 'EPSG:25832'; // EUREF89, UTM zone 32
 
-var matrixIds = [
+var matrixIdsKartverket = [
+  'EPSG:25832:0',
+  'EPSG:25832:1',
+  'EPSG:25832:2',
+  'EPSG:25832:3',
+  'EPSG:25832:4',
   'EPSG:25832:5',
-  //'EPSG:25832:6',
+  'EPSG:25832:6',
   'EPSG:25832:7',
-  //'EPSG:25832:8',
+  'EPSG:25832:8',
   'EPSG:25832:9',
-  //'EPSG:25832:10',
+  'EPSG:25832:10',
   'EPSG:25832:11',
-  //'EPSG:25832:12',
+  'EPSG:25832:12',
   'EPSG:25832:13',
   'EPSG:25832:14',
   'EPSG:25832:15',
   'EPSG:25832:16',
-  //'EPSG:25832:17'
+  'EPSG:25832:17'
 ];
+
+var matrixIds = [
+  matrixIdsKartverket[5],
+  matrixIdsKartverket[7],
+  matrixIdsKartverket[9],
+  matrixIdsKartverket[11],
+  matrixIdsKartverket[13],
+  matrixIdsKartverket[14],
+  matrixIdsKartverket[15],
+  matrixIdsKartverket[16]
+]
+
+var resolutions = [
+  resolutionsKartverket[5],
+  resolutionsKartverket[7],
+  resolutionsKartverket[9],
+  resolutionsKartverket[11],
+  resolutionsKartverket[13],
+  resolutionsKartverket[14],
+  resolutionsKartverket[15],
+  resolutionsKartverket[16]
+]
+
+var switchLayerResolution = resolutions[4];
 
 var grunnkart = new ol.layer.Tile({
   minResolution: switchLayerResolution,
@@ -96,49 +128,25 @@ var topo2 = new ol.layer.Tile({
   })
 });
 
-var bakgrunn = new ol.layer.Tile({
-  extent: extent150km,
-  source: new ol.source.TileWMS({
-    attributions: [attribution],
-    url: 'http://openwms.statkart.no/skwms1/wms.topo2.graatone?',
-    params: {
-      'LAYERS': 'topo2_graatone_WMS',
-      'STYLES': 'default'
-    },
-  })
-});
-
 var veger = new ol.layer.Tile({
   extent: extent150km,
   source: new ol.source.TileWMS({
     attributions: [attribution],
     url: 'http://openwms.statkart.no/skwms1/wms.topo2?',
     params: {
-      'LAYERS': 'N250Bilveg',
-      'STYLES': 'default'
-    },
-  })
-});
-
-var koter = new ol.layer.Tile({
-  extent: extent1200m,
-  source: new ol.source.TileWMS({
-    attributions: [attribution],
-    url: 'http://openwms.statkart.no/skwms1/wms.topo2?',
-    params: {
-      'LAYERS': 'fkb_hoydekurver_1m',
+      'LAYERS': 'N50Bilveg',
       'STYLES': 'default'
     },
   })
 });
 
 var map = new ol.Map({
-  layers: [grunnkart, topo2],
+  layers: [grunnkart, topo2, veger],
   target: 'map',
   view: new ol.View({
     projection: projection,
     center: [597017, 6732015],
     resolutions: resolutions,
-    zoom: 4
+    resolution: resolutions[3]
   })
 });
